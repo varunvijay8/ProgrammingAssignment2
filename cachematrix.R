547afecdf260f603bfd88c1ae570fc9bc75dbb5d
varunvijay8 <varunvijay8@gmail.com>
2015-01-25 15:57:47 -0800

    Change4

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a matrix object and returns list
+## of function:
+## 1. Sets the matrix and initializes inverse 
+## 2. Gets the matrix
+## 3. Sets inverse
+## 4. Gets inverse
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  if("MASS" %in% rownames(installed.packages()) == FALSE){
+    message("Install MASS....")
+    install.packages("MASS")
+    library("MASS")
+  }
+  
+  invMx <- NULL
+  set <- function(y) {
+    x <<- y
+    invMx <<- NULL
+  }
+  get <- function() x
+  setInv <- function(inv) invMx <<- inv
+  getInv <- function() invMx
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates and sets the inverse matrix value using
+## ginv funtion from MASS package
+## Installs MASS package if not already installed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  if("MASS" %in% rownames(installed.packages()) == FALSE){
+    message("Install MASS.....")
+    install.packages("MASS")
+    library("MASS")
+  }  
+  
+  invMx <- x$getInv()
+  
+  if(!is.null(invMx)) {
+    message("getting cached data")
+    return(invMx)
+  }
+  
+  data <- x$get()
+  invMx <- ginv(data)
+  x$setInv(invMx)
+  
+  invMx
 }
